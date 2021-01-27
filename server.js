f0a38d2fedc916bda87a8ce2d90d9bc5a37ecd28
menace31 <54880433+menace31@users.noreply.github.com>
2021-01-27 14:14:56 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,24 +1,30 @@
-let http = require('http') // import module et le stock dans la variable http
-let fs = require('fs')
-let url = require('url')
-const EventEmitter = require('events')
+let http = require('http'); // import module et le stock dans la variable http
 
+let fs = require('fs')
 
 let server = http.createServer()
 
-server.on('request', function(request, response) {
-    fs.readFile('index.html', (err,data) => {
-        if (err) {
-            response.writeHead(404)
-            response.end("Ce fichier n'existe pas sorry")
-        }
+server.on('request',(request,response) => {
+    fs.readFile('index.html',(err,data) => {
+        if (err) throw err
 
-        response.writeHead(200,{'Content-type': 'text/html; charset=utf-16'}) // permet de
+
         response.end(data)
     })
-})
 
-server.listen(8080)
+});
+
+server.listen(8080);
+
+
+
+
+
+
+
+
+
+
 
 
 
